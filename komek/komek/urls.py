1dfcaededd6ff641cb3bf0020a762733324338c2
Abylai-Yessim <ablai.shymside@gmail.com>
2023-12-06 18:00:11 +0600

    abc

--- komek/komek/urls.py
+++ komek/komek/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('back/', include('back.urls')),
+    path('', include('back.urls')),
     path('authe/', include('authe.urls'))
 ]
 
